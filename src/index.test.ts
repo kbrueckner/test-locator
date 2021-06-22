d2991f5e2fd1c5ed5af4dbda7d8b36296d6711c8
Brückner, Kenji <kenji.brueckner@gmail.com>
2021-06-22 20:11:21 +0200

    no message

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,33 @@
 /* eslint-env jest */
 
-import locator from './index';
+import locator, { format } from './index';
 
-describe('risk test-locator', () => {
-    test('simple locator', () => {
-        expect(locator('my-locator')).toEqual('my-locator');
+describe('test-locator', () => {
+    describe('format', () => {
+        test('to lowercase', () => {
+            expect(format('CaMeLcAsE')).toEqual('camelcase');
+        });
+
+        test('replace whitespaces', () => {
+            expect(format('my test  string')).toEqual('my-test-string');
+        });
     });
 
-    test('chained locator', () => {
-        expect(locator(['container', 'my-locator'])).toEqual('container__my-locator');
-    });
+    describe('locator', () => {
+        test('simple locator', () => {
+            expect(locator('my-locator')).toEqual('my-locator');
+        });
 
-    test('simple locator with element type', () => {
-        expect(locator('my-locator', 'button')).toEqual('my-locator--button');
-    });
+        test('chained locator', () => {
+            expect(locator(['container', 'my-locator'])).toEqual('container__my-locator');
+        });
 
-    test('chained locator with element type', () => {
-        expect(locator(['container', 'my-locator'], 'button')).toEqual('container__my-locator--button');
+        test('simple locator with element type', () => {
+            expect(locator('my-locator', 'button')).toEqual('my-locator--button');
+        });
+
+        test('chained locator with element type', () => {
+            expect(locator(['container', 'my-locator'], 'button')).toEqual('container__my-locator--button');
+        });
     });
 });
